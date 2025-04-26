b5d20701d05c480dd7fb36d2a41b6c9efb5164d2
Ashmit <ashmitkumar1020@gmail.com>
2025-04-26 08:48:55 +0530

    updated main.go file

--- autoship-server/cmd/server/main.go
+++ autoship-server/cmd/server/main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/ashmit123/Auto-Ship/autoship-server/internal/api"
+	"github.com/ashmit123/Auto-Ship/autoship-server/internal/db"
+	"github.com/ashmit123/Auto-Ship/autoship-server/internal/middleware"
 	"github.com/gofiber/fiber/v2"
-	"github.com/Ashmit-Kumar/Auto-Ship/autoship-server/internal/api"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Connect to MongoDB
+	db.Connect()
+	defer db.Disconnect()
+
 	app := fiber.New()
 
 	// Routes
-	app.Get("/ping", api.Ping)
+	app.Post("/signup", api.Signup)
+	app.Post("/login", api.Login)
 
-	log.Println("🚀 Server running at http://localhost:5000")
-	log.Fatal(app.Listen(":5000"))
+	// Protected routes
+	app.Get("/protected", middleware.IsAuthenticated, func(c *fiber.Ctx) error {
+		user := c.Locals("user")
+		return c.JSON(fiber.Map{"message": "Protected content", "user": user})
+	})
+
+	log.Println("🚀 Auto-Ship server running at http://localhost:3000")
+	log.Fatal(app.Listen(":3000"))
 }
