4f080bdde1e282ebd995dd49ab0295ae2ba4906a
Ashmit <ashmitkumar1020@gmail.com>
2025-04-29 14:41:56 +0530

    updating logo and name in login page

--- autoship-client/app/login/page.tsx
+++ autoship-client/app/login/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 import { LoginForm } from "@/components/login-form"
 import { Button } from "@/components/ui/button"
-import { Github } from "lucide-react"
+import { Github, ShipIcon } from "lucide-react"
 
 export default function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <Link href="/" className="absolute left-4 top-4 flex items-center gap-2 md:left-8 md:top-8">
-        <Github className="h-6 w-6" />
-        <span className="font-bold">GitHost</span>
+        <ShipIcon className="h-6 w-6" />
+        <span className="font-bold">AutoShip</span>
       </Link>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
