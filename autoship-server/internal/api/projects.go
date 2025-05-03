e227b1a0364e737f5760efe60e0832f590fc9f54
Ashmit <ashmitkumar1020@gmail.com>
2025-05-03 18:49:22 +0530

    update projects.go

--- autoship-server/internal/api/projects.go
+++ autoship-server/internal/api/projects.go
@@ -1,8 +1,9 @@
+// internal/api/projects.go
 package api
 
 import (
 	"strings"
-
+	// "fmt"
 	"github.com/Ashmit-Kumar/Auto-Ship/autoship-server/internal/services"
 	"github.com/Ashmit-Kumar/Auto-Ship/autoship-server/internal/db"
 	"github.com/Ashmit-Kumar/Auto-Ship/autoship-server/internal/models"
@@ -13,6 +14,48 @@ type RepoRequest struct {
 	URL      string `json:"url"`
 	Username string `json:"username"`
 }
+// func HandleRepoSubmit(c *fiber.Ctx) error {
+// 	var req RepoRequest
+// 	if err := c.BodyParser(&req); err != nil {
+// 		return fiber.NewError(fiber.StatusBadRequest, "Invalid request")
+// 	}
+
+// 	parts := strings.Split(strings.TrimSuffix(req.URL, ".git"), "/")
+// 	repoName := parts[len(parts)-1]
+
+// 	path, err := services.CloneRepository(req.URL, req.Username, repoName)
+// 	if err != nil {
+// 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+// 	}
+
+// 	projectType := services.DetectProjectType(path)
+// 	url := "/static/" + req.Username + "/" + repoName
+
+// 	// ✅ Auto deploy static sites to GitHub Pages
+// 	if projectType == "static" {
+// 		if err := services.PushToGitHubPages(req.Username, repoName); err != nil {
+// 			return fiber.NewError(fiber.StatusInternalServerError, "Failed to deploy to GitHub Pages: "+err.Error())
+// 		}
+// 	}
+
+// 	project := &models.Project{
+// 		Username:    req.Username,
+// 		RepoURL:     req.URL,
+// 		RepoName:    repoName,
+// 		ProjectType: projectType,
+// 		HostedURL:   url,
+// 	}
+
+// 	if err := db.SaveProject(project); err != nil {
+// 		return fiber.NewError(fiber.StatusInternalServerError, "Failed to save project")
+// 	}
+
+// 	return c.JSON(fiber.Map{
+// 		"message":     "Repository cloned and hosted",
+// 		"projectType": projectType,
+// 		"url":         url,
+// 	})
+// }
 
 func HandleRepoSubmit(c *fiber.Ctx) error {
 	var req RepoRequest
@@ -31,7 +74,13 @@ func HandleRepoSubmit(c *fiber.Ctx) error {
 
 	projectType := services.DetectProjectType(path)
 	url := "/static/" + req.Username + "/" + repoName
-
+	// var url string
+	// if projectType == "static" {
+	// 	url = fmt.Sprintf("https://%s.github.io/%s", req.Username, repoName)
+	// } else {
+	// 	url = "/static/" + req.Username + "/" + repoName
+	// }
+	
 	project := &models.Project{
 		Username:    req.Username,
 		RepoURL:     req.URL,
