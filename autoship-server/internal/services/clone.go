260fa7319a4022fb34decae1be064254438b04e5
Ashmit Kumar <ashmitkumar1020@gmail.com>
2025-07-12 17:56:37 +0530

    update clone.go

--- autoship-server/internal/services/clone.go
+++ autoship-server/internal/services/clone.go
@@ -27,9 +27,29 @@ func CloneRepository(repoURL, username, repoName string) (string, error) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		return "", fmt.Errorf("git clone failed: %v", err)
-	}
+	fmt.Println("Cloning repository:", repoURL)
+	// Run the command and check for errors
+	fmt.Println("Executing command:", cmd.String())
+	// cmd.Dir = "static" // Set working directory to static
+	// cmd.Env = append(os.Environ(), "GIT_TERMINAL_PROMPT=0")
+	// cmd.Env = append(cmd.Env, "GIT_ASKPASS=echo") // Disable password prompts
+	// cmd.Env = append(cmd.Env, "GIT_SSH_COMMAND=ssh -o StrictHostKeyChecking=no") // Disable strict host key checking
+	// cmd.Env = append(cmd.Env, "GIT_USER_NAME="+username) // Set username for commits
+	// cmd.Env = append(cmd.Env, "GIT_USER_EMAIL="+username+"@example.com") // Set email for commits
+	// cmd.Env = append(cmd.Env, "GIT_COMMITTER_NAME="+username) // Set committer name
+	// cmd.Env = append(cmd.Env, "GIT_COMMITTER_EMAIL="+username+"@example.com") // Set committer email
+	// cmd.Env = append(cmd.Env, "GIT_CONFIG_GLOBAL_USER_NAME="+username) // Set global user name
+	// cmd.Env = append(cmd.Env, "GIT_CONFIG_GLOBAL_USER_EMAIL="+username+"@example.com") // Set global user email
+	// cmd.Env = append(cmd.Env, "GIT_CONFIG_GLOBAL_INIT_DEFAULT_BRANCH=main") // Set default branch to main
+	// cmd.Env = append(cmd.Env, "GIT_CONFIG_GLOBAL_INIT_TEMPLATE_DIR=~/.git-templates") // Set global template directory
+	// cmd.Env = append(cmd.Env, "GIT_CONFIG_GLOBAL_INIT_COMMITTER_NAME="+username		) // Set global committer name
+
+	output, err := cmd.CombinedOutput()
+	fmt.Println("Git Output:\n", string(output))
+
+	if err != nil {
+	return "", fmt.Errorf("git clone failed: %v\n%s", err, output)
+ }
 	fmt.Println("Repository cloned successfully to:", path)
 	return path, nil
 }
