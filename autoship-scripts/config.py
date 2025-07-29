584f92264c086ef87541cb96a3e96979ec74110a
Ashmit Kumar <ashmitkumar1020@gmail.com>
2025-07-29 16:45:16 +0530

    update

--- autoship-scripts/config.py
+++ autoship-scripts/config.py
@@ -12,11 +12,11 @@ dotenv_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path)
 
 # Access environment variables
-GO_APP_HOST = os.getenv("GO_APP_HOST", "autoship-server")  # Docker hostname
-GO_APP_PORT = os.getenv("GO_APP_PORT", "3000")
+GO_APP_HOST = os.getenv("GO_APP_HOST")  # Docker hostname
+GO_APP_PORT = os.getenv("GO_APP_PORT")
 
 HOSTINGER_API_KEY = os.getenv("HOSTINGER_API_KEY")
-HOSTINGER_ZONE_ID = os.getenv("HOSTINGER_ZONE_ID")
+# HOSTINGER_ZONE_ID = os.getenv("HOSTINGER_ZONE_ID")
 HOSTINGER_BASE_DOMAIN = os.getenv("HOSTINGER_BASE_DOMAIN")
 
 NGINX_CONF_DIR = os.getenv("NGINX_CONF_DIR", "/etc/nginx/conf.d/")
