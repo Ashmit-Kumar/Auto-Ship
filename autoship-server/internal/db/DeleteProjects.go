2e8dcc2f146fcffb1235f27ac115b66c337b4975
Ayush Sharma <144806167+ayushsharma-1@users.noreply.github.com>
2025-08-01 04:11:33 +0000

    Fixing the database logic

--- autoship-server/internal/db/DeleteProjects.go
+++ autoship-server/internal/db/DeleteProjects.go
@@ -6,12 +6,11 @@ import (
     "go.mongodb.org/mongo-driver/bson"
 )
 
-// ...existing code...
 
 func DeleteProjectByContainerName(containerName string) error {
-    collection := Client.Database("autoship").Collection("projects")
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
-    _, err := collection.DeleteOne(ctx, bson.M{"containername": containerName})
-    return err
+	collection := GetCollection("projects")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := collection.DeleteOne(ctx, bson.M{"containername": containerName})
+	return err
 }
